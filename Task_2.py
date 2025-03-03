ef9e7a17989432807225a4fcdaa81048f4ad4b7b
Nadejda <zagzina2017@yandex.ru>
2025-03-03 19:07:49 +0300

    modified:   Task_2.py 	modified:   Task_3.py 	modified:   Task_4.py

--- Task_2.py
+++ Task_2.py
@@ -9,5 +9,6 @@ def words_index_map(strings: list) -> dict:
     return enumNames
 
 
+# Тест
 strings = ["hello world", "world of python", "hello again"]
 print(words_index_map(strings))
--- Task_4.py
+++ Task_4.py
@@ -1,11 +1,11 @@
 """Задача 4: Комплексные числа
 Напишите функцию, которая принимает список комплексных чисел
- и возвращает сумму модулей этих чисел."""
+и возвращает сумму модулей этих чисел."""
 
 
 def sum_of_moduli(complex_numbers: list) -> float:
     """Функция, которя принимает список комплексных чисел
-    и возвращает сумму модулей этих чисел"""
+    и возвращает сумму модулей этих чисел."""
     list_new = []
     for number in complex_numbers:
         list_new.append(abs(number))
@@ -13,5 +13,6 @@ def sum_of_moduli(complex_numbers: list) -> float:
     return sum_of_complex
 
 
+# Тест
 complex_numbers = [complex(3, 4), complex(1, 1), complex(0, 2)]
 print(sum_of_moduli(complex_numbers))
