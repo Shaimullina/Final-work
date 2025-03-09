42bca044148f25aaecd4a0fe76ee1efe96e4920a
Nadejda <zagzina2017@yandex.ru>
2025-03-09 16:08:18 +0300

    modified:   Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,17 +1,17 @@
 """Задача 1: Проверка матрицы на симметричность
-Напишите функцию, которая принимает на вход квадратную матрицу и проверяет, 
+Напишите функцию, которая принимает на вход квадратную матрицу и проверяет,
 является ли она симметричной относительно главной диагонали."""
 
 
 def is_symmetric(matrix: list) -> bool:
     flag = True
-    for i in range(matrix):
-        for j in range(matrix):
-            if matrix[i][j] != matrix[j][i] and i != j:
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            if matrix[i][j] != matrix[j][i]:
                 flag = False
                 break
-            if flag:
-                return flag
+        if flag:
+            return matrix
 
 
 matrix = [[1, 2, 3], [2, 4, 5], [3, 5, 6]]
